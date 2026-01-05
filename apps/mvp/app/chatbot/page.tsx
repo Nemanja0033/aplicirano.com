e18dd15f94c8452baa78e60fdb22c65080f2f722
Nemanja Antonijevic <antonijevicnemanja68@gmail.com>
2026-01-05 21:16:19 +0100

    add purchase button in navbar

--- apps/mvp/app/chatbot/page.tsx
+++ apps/mvp/app/chatbot/page.tsx
@@ -108,7 +108,7 @@ export default function ChatbotPage() {
   // }
 
   return (
-    <main className="w-full h-screen flex justify-center items-start overflow-auto">
+    <main className="w-full h-screen flex justify-center items-start">
       <div className="md:w-6xl w-full p-3 grid place-items-center gap-5">
         <div className="w-full dark:border-[#151046] dark:border-2 dark:bg-gradient-to-b from-[#100c28] to-[#010216] grid place-items-start p-5 rounded-lg shadow-md bg-white dark:bg-sidebar">
           <h1 className="text-2xl font-bold">AI Assistant</h1>
--- apps/mvp/components/Navbar.tsx
+++ apps/mvp/components/Navbar.tsx
@@ -1,9 +1,12 @@
 "use client"
+import { SparklesIcon } from 'lucide-react'
 import { ModeToggle } from './theme-toggler'
+import { Button } from './ui/button'
 
 const Navbar = () => {
   return (
-    <div className='absolute right-3 top-3'>
+    <div className='flex justify-end gap-2 items-center p-3 bg-transparent'>
+        <Button size={'lg'}>Nadogradi na <span className='flex items-center gap-1 text-cyan-300 animate-pulse'><SparklesIcon /> Pro</span></Button>
         <ModeToggle />
     </div>
   )
